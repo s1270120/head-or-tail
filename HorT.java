549eb26358c66172aabf453aabce66a566312c2f
s1270120 <s1270120@u-aizu.ac.jp>
2020-08-06 01:03:48 +0900

    HoT: Initial revision

---
+++ HorT.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+    
+public class HorT {
+    public static void main(String[] args){
+	
+System.out.println("Who are you? ");
+Scanner scan = new Scanner(System.in);
+String str = scan.next();
+System.out.println("Hello, "+str+"!");
+
+System.out.println("Tossing a coin...");
+String[] Ran ={"Heads","Tails"};
+int H=0,T=0;
+for(int i = 0;i<3; i++){
+    Random r = new Random();
+    String Ra = Ran [r.nextInt(2)];
+    //System.out.println(r);
+    if(Ra == "Heads") H = H+1;
+    else T=T+1;    
+    System.out.println("Round"+ (i+1) +":" + Ra);
+
+}
+System.out.println("Heads:"+H+",Tails:"+T);
+
+    }
+
+}
